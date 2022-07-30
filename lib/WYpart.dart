7aad2898260b37d7300b40029d497cb8a4bbace2
neotech67etkim <neotech67@gmail.com>
2022-07-30 12:41:32 +0900

    0730fuctionupdate

--- lib/WYpart.dart
+++ lib/WYpart.dart
@@ -1,50 +1,58 @@
 import 'package:container_fee_calculator/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class WoonimPage extends StatelessWidget {
   const WoonimPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('1/20 단계')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('운임 종류를 선택해 주세요.'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FeetPage()),
-                );
-              },
-              child: Text('안전위탁운임'),
+    return Consumer<CalculateService>(
+      builder: (context, service, child) {
+        return Scaffold(
+          appBar: AppBar(title: Text('1/20 단계')),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('운임 종류를 선택해 주세요.'),
+                ElevatedButton(
+                  onPressed: () {
+                    service.woonimCondition = "Wi";
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FeetPage()),
+                    );
+                  },
+                  child: Text('안전위탁운임'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    service.woonimCondition = "Woo";
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FeetPage()),
+                    );
+                  },
+                  child: Text('안전운송운임'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    service.woonimCondition = "User";
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FeetPage()),
+                    );
+                  },
+                  child: Text('운수사업자간운임'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CementLocationPage()),
-                );
-              },
-              child: Text('안전운송운임'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CementLocationPage()),
-                );
-              },
-              child: Text('운수사업자간운임'),
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
